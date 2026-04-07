9f31fda947003b133b3b3249074a8486914e3daf
Raj Shah <rajnshah2020@gmail.com>
2026-04-06 22:47:46 -0400

    Initial Commit

---
+++ BatteryUI/BatteryUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryUIApp.swift
+//  BatteryUI
+//
+//  Created by Raj Shah on 2026-04-06.
+//
+
+import SwiftUI
+
+@main
+struct BatteryUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
